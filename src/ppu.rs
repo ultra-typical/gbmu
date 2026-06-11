a3cf11d44990a659c2980f792c9d47b9040244be
syluiset <sylvain.arhkaz@gmail.com>
2026-06-11 17:15:40 +0200

    bug: nothing is displayed

--- src/ppu.rs
+++ src/ppu.rs
@@ -6,6 +6,7 @@ mod pixel_fifo;
 mod obj_piso;
 mod pixel_fetcher;
 mod oam_fetcher;
+mod vram;
 
 use crate::communications::GameCT;
 use crate::mmu::{MemoryMapper};
@@ -19,6 +20,7 @@ use crate::ppu::pixel_fifo::PixelFifo;
 use crate::ppu::obj_piso::ObjPiso;
 use crate::ppu::pixel_fetcher::PixelFetcher;
 use crate::ppu::oam_fetcher::OamFetcher;
+use crate::ppu::vram::{CgbVram, DmgVram, Vram};
 
 pub const WIN_SIZE_X: usize = 160;
 pub const WIN_SIZE_Y: usize = 144;
@@ -93,8 +95,8 @@ pub trait Ppu<M> {
     //Commons
     fn new() -> Self where Self: Sized;
     fn read_register(&self, addr: u16) -> u8;
-    fn read_vram(&mut self, addr: usize) -> u8;
-    fn write_vram(&mut self, addr: usize, data: u8);
+    fn read_vram(&mut self, addr: u16) -> u8;
+    fn write_vram(&mut self, addr: u16, data: u8);
     fn write_register(&mut self, addr: u16, val: u8);
     fn read_lcdc(&self) -> LcdControl {
         LcdControl::from_byte(self.lcdc_byte())
@@ -469,6 +471,7 @@ pub struct DmgPpu {
     // Pending interrupts to be drained by MMU after tick
     pub pending_vblank: bool,
     pub pending_stat: bool,
+    vram: DmgVram
 }
 
 pub struct CgbPpu {
@@ -510,7 +513,7 @@ pub struct CgbPpu {
     // Pending interrupts to be drained by MMU after tick
     pub pending_vblank: bool,
     pub pending_stat: bool,
-    vram: Vram
+    vram: CgbVram
 }
 
 impl<M> Ppu<M> for DmgPpu {
@@ -617,6 +620,7 @@ impl<M> Ppu<M> for DmgPpu {
             wx: 0x00,
             pending_vblank: false,
             pending_stat: false,
+            vram: DmgVram::new(),
         }
     }
 
@@ -637,11 +641,12 @@ impl<M> Ppu<M> for DmgPpu {
         }
     }
 
-    fn read_vram(&mut self, _addr: usize) -> u8 {
-        0
+    fn read_vram(&mut self, addr: u16) -> u8 {
+        self.vram.read(addr)
     }
 
-    fn write_vram(&mut self, _addr: usize, _data: u8) {
+    fn write_vram(&mut self, addr: u16, data: u8) {
+        self.vram.write(addr, data);
     }
 
     fn write_register(&mut self, addr: u16, val: u8) {
@@ -940,16 +945,16 @@ impl<M> Ppu<M> for CgbPpu {
             wx: 0x00,
             pending_vblank: false,
             pending_stat: false,
-            vram: Vram::new()
+            vram: CgbVram::new()
         }
     }
 
-    fn write_vram(&mut self, addr: usize, val: u8) {
-        self.vram.write_byte(addr, val);
+    fn write_vram(&mut self, addr: u16, val: u8) {
+        self.vram.write(addr, val);
     }
 
-    fn read_vram(&mut self, addr: usize) -> u8 {
-        self.vram.read_byte(addr)
+    fn read_vram(&mut self, addr: u16) -> u8 {
+        self.vram.read(addr)
     }
 
     fn read_register(&self, addr: u16) -> u8 {
@@ -1046,40 +1051,6 @@ impl<M> Ppu<M> for CgbPpu {
                     break;
                 }
             }
-        }
-    }
-}
-
-struct Vram {
-    bank0: Box<[u8; 0x1FFF]>,
-    bank1: Box<[u8; 0x1FFF]>,
-    vbk: u8,        // 0xFF4F
-}
-
-impl Vram {
-    fn new() -> Vram {
-        Vram {
-            bank0: Box::new([0x00; 0x1FFF]),
-            bank1: Box::new([0x00; 0x1FFF]),
-            vbk: 0x00
-        }
-    }
-
-    fn write_byte(&mut self, addr: usize, val: u8) {
-        let addr_in_banks = addr - 0x8000;
-        match self.vbk {
-            0x00 => self.bank0[addr_in_banks] = val,
-            0x01 => self.bank1[addr_in_banks] = val,
-            _ => {}
-        }
-    }
-
-    fn read_byte(&mut self, addr: usize) -> u8 {
-        let addr_in_banks = addr - 0x8000;
-        match self.vbk {
-            0x00 => self.bank0[addr_in_banks],
-            0x01 => self.bank1[addr_in_banks],
-            _ => unreachable!()
         }
     }
 }
