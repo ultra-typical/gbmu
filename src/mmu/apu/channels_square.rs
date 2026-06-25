3db486ea1b2e3e8584ef8664a18200be11c45ee2
LezardSC <lezardsouscultive@gmail.com>
2026-06-25 20:48:45 +0200

    feat: implement sweep functionality for channel square

--- src/mmu/apu/channels_square.rs
+++ src/mmu/apu/channels_square.rs
@@ -21,11 +21,71 @@ pub struct ChannelSquare {
     length_counter: u8,
     volume: u8,
     envelope_timer: u8,
+    shadow_frequency: u16,
+    sweep_timer: u8,
+    sweep_enabled: bool,
 }
 
 impl ChannelSquare {
     fn dac_enabled(&self) -> bool {
         (self.nr2_volume_envelope.raw() & 0b1111_1000) != 0
+    }
+
+    fn sweep_shift(&self) -> u8 {
+        self.nr0_sweep.raw() & 0b0000_0111
+    }
+
+    fn sweep_period(&self) -> u8 {
+        (self.nr0_sweep.raw() >> 4) & 0b0000_0111
+    }
+
+    fn sweep_negate(&self) -> bool {
+        self.nr0_sweep.raw() & 0b0000_1000 != 0
+    }
+
+    fn calculate_sweep(&self) -> u16 {
+        let delta = self.shadow_frequency >> self.sweep_shift();
+
+        if self.sweep_negate() {
+            self.shadow_frequency - delta
+        } else {
+            self.shadow_frequency + delta
+        }
+    }
+
+    fn write_frequency(&mut self, freq: u16) {
+        self.nr3_period_low.write((freq & 0xFF) as u8);
+
+        let high = ((freq >> 8) & 0b0111) as u8;
+        let nr14 = self.nr4_period_high_ctrl.raw() & 0b1111_1000;
+        self.nr4_period_high_ctrl.write(nr14 | high);
+    }
+
+    fn tick_sweep(&mut self) {
+        if self.sweep_timer > 0 {
+            self.sweep_timer -= 1;
+        }
+
+        if self.sweep_timer == 0 {
+            self.sweep_timer = if self.sweep_period() != 0 {
+                self.sweep_period()
+            } else {
+                8
+            };
+
+            if self.sweep_enabled && self.sweep_period() != 0 {
+                let new_frequency = self.calculate_sweep();
+
+                if new_frequency > 2047 {
+                    self.enabled = false;
+                } else if self.sweep_shift() != 0 {
+                    self.shadow_frequency = new_frequency;
+
+                    self.write_frequency(new_frequency);
+                    if self.calculate_sweep() > 2047 { self.enabled = false; }
+                }
+            }
+        }
     }
 
     pub fn tick_length(&mut self) {
@@ -72,6 +132,17 @@ impl ChannelSquare {
 
         self.volume = (self.nr2_volume_envelope.raw() & 0b1111_0000) >> 4;
         self.envelope_timer = self.nr2_volume_envelope.raw() & 0b0000_0111;
+
+        self.shadow_frequency = self.period();
+        self.sweep_timer = self.sweep_period();
+        if self.sweep_timer == 0 { self.sweep_timer = 8; }
+        self.sweep_enabled = self.sweep_period() != 0 || self.sweep_shift() != 0;
+
+        // if shift != 0, immediately calculate then check overflow at trigger (hardware behavior)
+        if self.sweep_shift() != 0 {
+            let new_freq = self.calculate_sweep();
+            if new_freq > 2047 { self.enabled = false; }
+        }
     }
 
     fn period(&self) -> u16 {
