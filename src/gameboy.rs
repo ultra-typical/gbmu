5bf539fcad283895511659f0da21e835350ceb97
Axiaaa <lucie.camerlynck@hotmail.com>
2026-06-26 19:50:20 +0200

    fix: DMA bug

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -318,10 +318,11 @@ impl<M: MemoryMapper> GameBoy<M> {
         self.cycles_elapsed += 1;
 
         if self.cycles_elapsed.is_multiple_of(4) {
-            if self.bus.get_dma_index() != 0xFF {
+            if self.bus.dma_active() {
                 self.bus.tick_dma();
+            } else {
+                self.cpu.tick(&mut self.bus);
             }
-            self.cpu.tick(&mut self.bus);
             self.cycles_elapsed = 0;
         }
 
